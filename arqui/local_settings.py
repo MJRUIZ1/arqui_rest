bb9d0353b71020a988d462b578ba32aac06f2163
MJRUIZ1 <173193@ids.upchiapas.edu.mx>
2019-05-31 13:48:46 -0500

    heroku2

--- arqui/local_settings.py
+++ arqui/local_settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'arqui.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'DB_HOST',
-        'PORT': 'DB_PORT'
+        'NAME' : 'arquidb',
+        'USER': 'postgres',
+        'PASSWORD': 'Pandora',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
