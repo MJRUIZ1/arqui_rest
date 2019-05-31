ecf986b9ddb6af2cd8f7cff680d796356fc7c6c7
MJRUIZ1 <173193@ids.upchiapas.edu.mx>
2019-05-31 16:08:09 -0500

    esta si es la vencida

--- arqui/settings.py
+++ arqui/settings.py
@@ -146,6 +146,6 @@ if not DEBUG:
             'PORT': 'DB_PORT'
         }
     } 
-
-   # import django_heroku
-#django_heroku.settings(locals())
+    
+    import django_heroku
+    django_heroku.settings(locals())
