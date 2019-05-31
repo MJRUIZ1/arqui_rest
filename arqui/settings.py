1429da0784d08aec290c5d552e586af1411e3648
MJRUIZ1 <173193@ids.upchiapas.edu.mx>
2019-05-31 16:41:12 -0500

    este si ahora si

--- arqui/settings.py
+++ arqui/settings.py
@@ -137,14 +137,14 @@ except ImportError:
 if not DEBUG:
     SECRET_KEY = 'SECRET_KEY'
     DATABASES = {
-        'default': {
-            'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-            'NAME' : 'DB_NAME',
-            'USER': 'DB_USER',
-            'PASSWORD': 'DB_PASSWORD',
-            'HOST': 'DB_HOST',
-            'PORT': 'DB_PORT'
-        }
+    'default': {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'DB_HOST',
+         'PORT': 'DB_PORT'
+      }
     } 
     
     import django_heroku
